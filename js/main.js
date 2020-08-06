22322bd1ac7772f97b975d848cd26092e7425b5e
M. Raka Maulana A <m.rakamaulanaalan@studentpartner.com>
2020-08-07 06:13:02 +0700

    Update log notifikasi

--- js/main.js
+++ js/main.js
@@ -42,37 +42,11 @@ function requestPermission() {
 									),
 								})
 								.then(function (subscribe) {
-									console.log(
-										"Berhasil melakukan subscribe dengan endpoint: ",
-										subscribe.endpoint
-									);
-									console.log(
-										"Berhasil melakukan subscribe dengan p256dh key: ",
-										btoa(
-											String.fromCharCode.apply(
-												null,
-												new Uint8Array(
-													subscribe.getKey("p256dh")
-												)
-											)
-										)
-									);
-									console.log(
-										"Berhasil melakukan subscribe dengan auth key: ",
-										btoa(
-											String.fromCharCode.apply(
-												null,
-												new Uint8Array(
-													subscribe.getKey("auth")
-												)
-											)
-										)
-									);
+									console.log("Berhasil melakukan subscribe");
 								})
 								.catch(function (e) {
 									console.error(
-										"Tidak dapat melakukan subscribe ",
-										e.message
+										"Tidak dapat melakukan subscribe "
 									);
 								});
 						});
